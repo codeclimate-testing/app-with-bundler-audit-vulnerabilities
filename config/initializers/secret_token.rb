87ca40e7bef5074626ccd85badd5c9a5a9632699
Max Jacobson <max@hardscrabble.net>
2017-03-13 17:35:06 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppWithVulnerabilities::Application.config.secret_token = '6e314abe2fec1fbddffe1488f23bc73157fb87e6cbede30532fc44fb53926f70383a339e67d5371851c9261a2bb24dd479c8040d74b3e44df1734749c58c2e6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AppWithVulnerabilities::Application.config.session_store :cookie_store, key: '_app-with-vulnerabilities_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AppWithVulnerabilities::Application.config.session_store :active_record_store
